a86832aeece1f747fa33586f68b6319160c36cb3
darwich6 <adarwich2010@gmail.com>
2024-09-18 10:28:22 -0500

    feat: navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import Navbar from "~/app/_components/navbar";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -14,7 +15,10 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
