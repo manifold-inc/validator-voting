9e50566918a06ad15edbd5c817877983056bf518
darwich6 <adarwich2010@gmail.com>
2024-09-19 11:53:12 -0500

    feat: remove card

--- src/app/staking/page.tsx
+++ src/app/staking/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { RocketLaunchIcon } from "@heroicons/react/24/solid";
 import { useState, useEffect } from "react";
 import { env } from "~/env.mjs";
 import { useWalletStore } from "~/providers/wallet-store-provider";
@@ -21,9 +20,9 @@ export default function Staking() {
 
   return (
     <div className="relative p-4">
-      <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         {/* Form container with gradient background */}
-        <div className="relative shadow-xl sm:rounded-2xl overflow-hidden">
+        <div className="relative overflow-hidden shadow-xl sm:rounded-2xl">
           {/* Gradient background on the form */}
           <div className="absolute inset-0">
             <div
@@ -36,9 +35,9 @@ export default function Staking() {
           {/* Form content */}
           <div className="relative px-6 py-2 sm:py-4 lg:px-6 lg:py-12">
             <form className="mx-auto max-w-3xl">
-              <div className="space-y-6 border rounded-lg p-6 sm:space-y-16 bg-white bg-opacity-80 shadow-xl">
+              <div className="space-y-6 rounded-lg border p-6 shadow-xl sm:space-y-16">
                 <div>
-                  <h2 className="font-semibold leading-7 text-gray-900">
+                  <h2 className="font-semibold leading-7 text-white">
                     Delegate to {env.NEXT_PUBLIC_VALIDATOR_NAME}
                   </h2>
 
@@ -46,18 +45,16 @@ export default function Staking() {
                     <div className="sm:grid sm:grid-cols-3 sm:items-center sm:gap-4 sm:py-6">
                       <label
                         htmlFor="validatorAddress"
-                        className="block font-medium leading-6 text-gray-900 sm:pt-1.5"
+                        className="block font-medium leading-6 text-white sm:pt-1.5"
                       >
                         Validator Address:
                       </label>
-                      <span className="mt-2 text-gray-600 sm:col-span-2 sm:mt-0">
+                      <span className="mt-2 text-gray-200 sm:col-span-2 sm:mt-0">
                         <span className="hidden sm:inline">
-                          {(env.NEXT_PUBLIC_VALIDATOR_ADDRESS)}
+                          {env.NEXT_PUBLIC_VALIDATOR_ADDRESS}
                         </span>
                         <span className="inline sm:hidden">
-                          {truncateAddress(
-                            env.NEXT_PUBLIC_VALIDATOR_ADDRESS
-                          )}
+                          {truncateAddress(env.NEXT_PUBLIC_VALIDATOR_ADDRESS)}
                         </span>
                       </span>
                     </div>
@@ -65,19 +62,17 @@ export default function Staking() {
                     <div className="sm:grid sm:grid-cols-3 sm:items-center sm:gap-4 sm:py-6">
                       <label
                         htmlFor="yourAddress"
-                        className="block font-medium leading-6 text-gray-900 sm:pt-1.5"
+                        className="block font-medium leading-6 text-white sm:pt-1.5"
                       >
                         Your Address:
                       </label>
-                      <span className="mt-2 text-gray-600 sm:col-span-2 sm:mt-0">
+                      <span className="mt-2 text-gray-200 sm:col-span-2 sm:mt-0">
                         <span className="hidden sm:inline">
-                          {(connectedAccount)}
+                          {connectedAccount}
                         </span>
                         <span className="inline sm:hidden">
-                          {connectedAccount && 
-                          truncateAddress(
-                            connectedAccount
-                          )}
+                          {connectedAccount &&
+                            truncateAddress(connectedAccount)}
                         </span>
                       </span>
                     </div>
@@ -85,11 +80,11 @@ export default function Staking() {
                     <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
                       <label
                         htmlFor="staked"
-                        className="block font-medium leading-6 text-gray-900 sm:pt-1.5"
+                        className="block font-medium leading-6 text-white sm:pt-1.5"
                       >
                         Staked:
                       </label>
-                      <div className="mt-2 flex items-center justify-between text-gray-600 sm:col-span-2 sm:mt-0">
+                      <div className="mt-2 flex items-center justify-between text-gray-200 sm:col-span-2 sm:mt-0">
                         <span>0.000 Tao (0.00$)</span>
                         <button className="ml-auto rounded bg-gray-200 px-2 py-1 text-gray-700 hover:bg-gray-300">
                           MAX
@@ -100,11 +95,11 @@ export default function Staking() {
                     <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
                       <label
                         htmlFor="available"
-                        className="block font-medium leading-6 text-gray-900 sm:pt-1.5"
+                        className="block font-medium leading-6 text-white sm:pt-1.5"
                       >
                         Available:
                       </label>
-                      <div className="mt-2 flex items-center justify-between text-gray-600 sm:col-span-2 sm:mt-0">
+                      <div className="mt-2 flex items-center justify-between text-gray-200 sm:col-span-2 sm:mt-0">
                         <span>0.000 Tao (0.00$)</span>
                         <button className="ml-auto rounded bg-gray-200 px-2 py-1 text-gray-700 hover:bg-gray-300">
                           MAX
@@ -115,7 +110,7 @@ export default function Staking() {
                     <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
                       <label
                         htmlFor="taoAmount"
-                        className="block font-medium leading-6 text-gray-900 sm:pt-1.5"
+                        className="block font-medium leading-6 text-white sm:pt-1.5"
                       >
                         Tao Amount:
                       </label>
@@ -132,31 +127,31 @@ export default function Staking() {
                               setTaoAmount(value);
                             }
                           }}
-                          className="flex border border-gray-500 rounded bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-500 focus:ring-0 sm:text-sm sm:leading-6"
+                          className="flex rounded border border-gray-200 bg-transparent py-1.5 pl-1 text-white placeholder:text-gray-200 focus:ring-0 sm:text-sm sm:leading-6"
                         />
                       </div>
                     </div>
                   </div>
                 </div>
                 <div className="flex justify-center gap-4">
-                <button
-                  type="submit"
-                  className={`flex items-center justify-center gap-2 w-40 rounded-md border border-transparent bg-indigo-500 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-600 sm:px-8 ${
-                    isFormValid ? "" : "cursor-not-allowed opacity-60"
-                  }`}
-                  disabled={!isFormValid}
-                >
-                  Delegate
-                </button>
-                <button
-                  type="submit"
-                  className={`flex items-center justify-center gap-2 w-40 rounded-md border border-transparent bg-indigo-500 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-600 sm:px-8 ${
-                    isFormValid ? "" : "cursor-not-allowed opacity-60"
-                  }`}
-                  disabled={!isFormValid}
-                >
-                  Undelegate
-                </button>
+                  <button
+                    type="submit"
+                    className={`flex w-40 items-center justify-center gap-2 rounded-md border border-transparent bg-indigo-500 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-600 sm:px-8 ${
+                      isFormValid ? "" : "cursor-not-allowed opacity-60"
+                    }`}
+                    disabled={!isFormValid}
+                  >
+                    Delegate
+                  </button>
+                  <button
+                    type="submit"
+                    className={`flex w-40 items-center justify-center gap-2 rounded-md border border-transparent bg-indigo-500 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-600 sm:px-8 ${
+                      isFormValid ? "" : "cursor-not-allowed opacity-60"
+                    }`}
+                    disabled={!isFormValid}
+                  >
+                    Undelegate
+                  </button>
                 </div>
               </div>
             </form>
